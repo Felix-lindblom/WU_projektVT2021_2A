f4abaca8ce7f24f97e1caeb5719bc72e87f1d01c
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2021-03-01 17:02:58 +0100

    0.6.0 baksteget
    känns som jag rör mig bak och kan hända jag går bak en version  för mycket lägger inte ihop sig.
    Tror det mesta är kodat utan tanken hur annat ska vara med det

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+const mobilNav = document.querySelector('.mobil-nav')
+const circle = document.querySelector('circle')
+
 function toggleMeny(){
     document.querySelector('.mobil-nav').classList.toggle('nav-active')
     document.querySelector('.circle').classList.toggle('toggle')
 }
 
+document.querySelector('.circle').addEventListener('click', toggleMeny)
