711770ae9faf4c686c4bd56c2f590a2d3fe49fab
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2021-03-07 21:37:04 +0100

    0.9.0 eller kan det vara 0.8.0
    fixade lite kod relterat till nav. Sedan la jag till lite mer still och style på andra element så det ser mer ut som jag tänkte mig.  Utsennade  så är det nära till det jag vill.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const mobilNav = document.querySelector('.mobil-nav')
-const circle = document.querySelector('circle')
+// gör klart mig!!!
+
+const Nav = document.querySelector('.mobil-nav')
+const circle = document.querySelector('.circle')
 
 function toggleMeny(){
-    document.querySelector('.mobil-nav').classList.toggle('nav-active')
-    document.querySelector('.circle').classList.toggle('toggle')
+    Nav.classList.toggle('.nav-active')
+    circle.classList.toggle('toggle')
 }
 
-document.querySelector('.circle').addEventListener('click', toggleMeny)
+circle.addEventListener('click', toggleMeny)
