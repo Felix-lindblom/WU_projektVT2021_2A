12e0ee5dac8b375ca93a4657b37899884b783b24
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2021-03-19 17:40:06 +0100

    0.13.0 sidan hemsida på mobil är klar
    faktiskt klar med mobil sidan, är bara lite kanske med transtion

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// gör klart mig!!!
 const nav = document.querySelector('.nav-link')
-const circle = document.querySelector('.nav-but')
+const circle = document.querySelector('#bak-circle')
+const cross = document.querySelector('#cross')
 
 function toggleMeny() {
     nav.classList.toggle('nav-active')
     circle.classList.toggle('toggle')
+    cross.classList.toggle('toggle')
 }
 
 circle.addEventListener('click', toggleMeny)
+cross.addEventListener('click', toggleMeny)
